2e771850dc36df7aa3552ec0343c7cba0c1a7631
mxxghara <mxxghara@uwaterloo.ca>
2024-03-24 20:46:41 -0400

    update text display

---
+++ textdisplay.cc
@@ -0,0 +1,46 @@
+#include "textdisplay.h"
+
+TextDisplay::TextDisplay(int w, int h, int lvl): boardWidth{w}, boardHeight{h}, 
+  level{lvl} {
+  vector<char> row(boardWidth, ' ');
+  for (int i = 0; i < boardHeight; ++i) {
+    theDisplay.emplace_back(row);
+  }
+}
+
+void TextDisplay::notify(Block &b) {
+  
+}
+
+TextDisplay::~TextDisplay() {}
+
+ostream &operator<<(ostream &out, const TextDisplay &td) {
+  // print level and score
+  // string level = "Level:";
+  // string score = "Score:";
+  // cout << level;
+  // for (int i = level.size(); i < td.boardWidth - 1; ++i) {
+  //   cout << ' ' << td.level << endl;
+  // }
+  // cout << score;
+  // for (int i = score.size(); i < td.boardWidth - 1; ++i) {
+
+  // }
+
+  // print border
+  for (int i = 0; i < td.boardWidth; ++i) { cout << '-'; }
+
+  // print out the game board
+  for (int i = 0; i < td.boardHeight; ++i) {
+    if (i >= td.reserved) { cout << '|'; }
+    for (int j = 0; j < td.boardWidth; ++j) {
+      cout << td.theDisplay[i][j];
+    }
+    cout << endl;
+  }
+  // print the bottom border
+  cout << '+';
+  for (int i = 0; i < td.boardWidth; ++i) { cout << '-'; }
+  cout << endl;
+  return out;
+}
--- textdisplay.h
+++ textdisplay.h
@@ -1,8 +1,23 @@
 #ifndef __TEXTDISPLAY_H__
 #define __TEXTDISPLAY_H__
+#include <iostream>
+#include <vector>
+#include "observer.h"
+using namespace std;
 
-class TextDisplay {
-  
+class Block;
+
+class TextDisplay : public Observer {
+    vector<vector<char>> theDisplay;
+    const int boardWidth;
+    const int boardHeight;
+    const int reserved = 3;
+    const int level;
+  public:
+    TextDisplay(int w, int h, int lvl);
+    void notify(Block &b) override;
+    ~TextDisplay();
+    friend std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
 };
 
 #endif
