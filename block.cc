7d3380447f2a15d4d5c325e180f0c79861a517e8
j37shen <j37shen@uwaterloo.ca>
2024-03-25 14:23:14 -0400

    beginning block impl

--- block.cc
+++ block.cc
@@ -1,1 +1,26 @@
 #include "block.h"
+
+// recheck if this constructor is enough
+Block::Block() : row {0}, col {0} {}
+
+void Block::setCoords(int row, int col) {
+    this->row = row;
+    this->col = col;
+}
+
+int Block::getRow() const {
+    return row;
+}
+int Block::getCol() const {
+    return col;
+}
+
+// implementation ideas:
+//  *field for checking for blocks remaining, notify that turn is being made, and turn with same amount
+void Block::notifyObservers() {
+
+}
+
+void Block::attatch(Observer *o) {
+    observers.emplace_back(0);
+}
--- block.h
+++ block.h
@@ -3,14 +3,28 @@
 #include <iostream>
 #include <vector>
 #include "observer.h"
+using namespace std; 
 
 class Block : public Observer {
-    int row;
-    int col;
+    int row, col;
+    vector<Observer*> observers;
+
   public:
-    // add methods
-    // add public method/accessor for getting the block type, 
-    // similar to getState in the example
+    Block(); // default constructor
+
+    void setCoords(int row, int col);
+    int getRow() const;
+    int getCol() const;
+
+    // made virtual in block class since each subclass will set which type of block
+    virtual BlockType bType() override;
+
+    void notifyObservers(); 
+    void attatch(Observer *o);
+
+    virtual void rotateBlock(bool clockwise) = 0;
+    virtual void shiftBlock(string dir) = 0;
+    
 };
 
 #endif
