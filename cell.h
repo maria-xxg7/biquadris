bb79cc2a1a3f55f4a8ead04c48d6dc81eaf299b8
mxxghara <mxxghara@uwaterloo.ca>
2024-03-25 16:34:10 -0400

    cell updates

--- cell.h
+++ cell.h
@@ -1,9 +1,25 @@
 #ifndef __CELL_H__
 #define __CELL_H__
+#include <vector>
+#include "observer.h"
+using namespace std;
 
-class Cell {
+class Cell : public Observer {
+    bool isOn = false;
+    vector<Observer*> observers;
+    int row, col;
+  public:
+    Cell();
+    bool getState() const;
+    int getRow() const;
+    int getCol() const;
+    void setOn();
+    void setCoords(int r, int c);
+    void attach(Observer *o);
 
-
+    BlockType bType() override;
+    void notify(Cell &c) override;
+    void notifyAllObservers();
 };
 
 #endif
--- observer.h
+++ observer.h
@@ -1,14 +1,14 @@
 #ifndef _OBSERVER_H_
 #define _OBSERVER_H_
 
-class Block;
+class Cell;
 
 enum class BlockType { IBlock, JBlock, LBlock, OBlock, SBlock, ZBlock, TBlock };
 
 class Observer {
   public:
-    // b is a Block that called the notify method so we can get its position
-    virtual void notify(Block &b) = 0;
+    // c is a cell that called the notify method so we can get its position
+    virtual void notify(Cell &c) = 0;
     virtual BlockType bType() = 0;
     virtual ~Observer() = default;
 };
