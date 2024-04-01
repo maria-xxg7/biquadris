d138fb1becf94890120d422334066acbb741b130
j37shen <j37shen@uwaterloo.ca>
2024-03-31 21:36:55 -0400

    tests and player

--- levelone.h
+++ levelone.h
@@ -1,9 +1,9 @@
 #include "player.h"
 
-// class LevelOne: public Player {
-//   string filename;
-//   LevelOne(bool playerNum, int level, Board &b, string filename);
-//   public:
-//     void newMove(Board &b, bool playerTurn) override; 
-
-// };
+class LevelOne: public Player {
+  string filename;
+  int counter = 0;
+  LevelOne(bool playerNum, int level, Board &b, string filename);
+  public:
+    void newMove(Board &b, bool playerTurn) override; 
+};
--- player.h
+++ player.h
@@ -1,21 +1,25 @@
-// #ifndef __PLAYER_H__
-// #define __PLAYER_H__
-// #include <iostream>
-// #include "block.h"
-// #include "board.h"
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "block.h"
+#include "board.h"
 
-// class Player {
-//     bool playerNum;
-//     int level;
-//     BlockType nextBlock;
-//     Board *playerBoard;
-//   protected:
-//     Player(bool playerNum, int level, Board &b);
-//     ~Player();
+class Player {
+    bool playerNum;
+    int level;
+    BlockType nextBlock;
+    Board *playerBoard;
+  protected:
+    Player(bool playerNum, int level, Board &b);
+    ~Player();
+    BlockType stringToBlock(char blockChar);
 
-//   public:
-//     virtual void newMove(Board &b, bool playerTurn) = 0;
-//     // level up and level down be decalred here i think
-// };
+  public:
+    virtual void newMove(Board &b, bool playerTurn) = 0;
+    void levelUp(Board &b, bool playerTurn);
+    void levelDown(Board &b, bool playerTurn);
+};
 
-// #endif
+#endif
