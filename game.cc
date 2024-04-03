b3835b9b1bda54d1b2721985bc21f9ea56363503
j37shen <j37shen@uwaterloo.ca>
2024-04-02 22:45:33 -0400

    game

--- game.cc
+++ game.cc
@@ -1,144 +1,144 @@
-// #include "game.h"
-// using namespace std;
+#include "game.h"
+using namespace std;
 
-// // adding textdisplay and graphics display to this
+// adding textdisplay and graphics display to this
 
-// Game::Game(Xwindow &wd1, Xwindow &wd2) : playerTurn {0} {
-//   shared_ptr<Board> boardPlayer1 = make_shared<Board>();
-//   shared_ptr<Board> boardPlayer2 = make_shared<Board>();
+Game::Game(Xwindow &wd1, Xwindow &wd2) : playerTurn {0} {
+  shared_ptr<Board> boardPlayer1 = make_shared<Board>();
+  shared_ptr<Board> boardPlayer2 = make_shared<Board>();
 
-//   shared_ptr<BlockGenerator> levelPlayer1 = make_shared<LevelZero>("sequence1.txt");
-//   shared_ptr<BlockGenerator> levelPlayer2 = make_shared<LevelZero>("sequence2.txt");
+  shared_ptr<BlockGenerator> levelPlayer1 = make_shared<LevelZero>("sequence1.txt");
+  shared_ptr<BlockGenerator> levelPlayer2 = make_shared<LevelZero>("sequence2.txt");
   
-//   playerBoards.emplace_back(boardPlayer1);
-//   playerBoards.emplace_back(boardPlayer2);
-//   playerBoards[0]->init(wd1);
-//   playerBoards[1]->init(wd2);
+  playerBoards.emplace_back(boardPlayer1);
+  playerBoards.emplace_back(boardPlayer2);
+  playerBoards[0]->init(wd1);
+  playerBoards[1]->init(wd2);
 
-//   playerLevels.emplace_back(levelPlayer1);
-//   playerLevels.emplace_back(levelPlayer2);
-// }
+  playerLevels.emplace_back(levelPlayer1);
+  playerLevels.emplace_back(levelPlayer2);
+}
 
-// void Game::restartGame() {
-//   playerBoards[0]->clearBoard();
-//   playerBoards[1]->clearBoard();
+void Game::restartGame() {
+  playerBoards[0]->clearBoard();
+  playerBoards[1]->clearBoard();
 
-//   playerTurn = 0;
-// }
+  playerTurn = 0;
+}
 
-// void Game::startGame() {
-//   playerLevels[0]->newMove(playerBoards[0]);
-//   playerBoards[0]->moveBlock("");
-//   playerLevels[0]->newMove(playerBoards[0]);
-//   cout << "PLAYER 1: " << endl;
-//   cout << *playerBoards[0] << endl;
-//   playerLevels[1]->newMove(playerBoards[1]);
-//   playerBoards[1]->moveBlock("");
-//   playerLevels[1]->newMove(playerBoards[1]);
-//   cout << "PLAYER 2: " << endl;
-//   cout << *playerBoards[1] << endl;
-// }
+void Game::startGame() {
+  playerLevels[0]->newMove(playerBoards[0]);
+  playerBoards[0]->moveBlock("");
+  playerLevels[0]->newMove(playerBoards[0]);
+  cout << "PLAYER 1: " << endl;
+  cout << *playerBoards[0] << endl;
+  playerLevels[1]->newMove(playerBoards[1]);
+  playerBoards[1]->moveBlock("");
+  playerLevels[1]->newMove(playerBoards[1]);
+  cout << "PLAYER 2: " << endl;
+  cout << *playerBoards[1] << endl;
+}
 
-// void Game::levelUp() {
-//   int playerLevel = playerBoards[playerTurn]->getLevel();
-//   if ( playerLevel == 4) {
-//     return;
-//   } else {
-//     if (playerLevel == 0) {
-//       if (playerTurn) {
-//         playerLevels[playerTurn] = make_shared<LevelOne>();
-//       } else {
-//         playerLevels[playerTurn] = make_shared<LevelOne>();
-//       }
-//     } else if (playerLevel == 1) {
-//       playerLevels[playerTurn] = make_shared<LevelTwo>();
-//     } else if (playerLevel == 2) {
-//       playerLevels[playerTurn] = make_shared<LevelHard>(3);
-//       playerBoards[playerTurn]->setHeavy(true);
-//     } else {
-//       playerLevels[playerTurn] = make_shared<LevelHard>(4);
-//       playerBoards[playerTurn]->setObstacle(true);
-//     }
-//   }
-//   playerBoards[playerTurn]->setLevel(playerLevel + 1);
-// }
+void Game::levelUp() {
+  int playerLevel = playerBoards[playerTurn]->getLevel();
+  if ( playerLevel == 4) {
+    return;
+  } else {
+    if (playerLevel == 0) {
+      if (playerTurn) {
+        playerLevels[playerTurn] = make_shared<LevelOne>();
+      } else {
+        playerLevels[playerTurn] = make_shared<LevelOne>();
+      }
+    } else if (playerLevel == 1) {
+      playerLevels[playerTurn] = make_shared<LevelTwo>();
+    } else if (playerLevel == 2) {
+      playerLevels[playerTurn] = make_shared<LevelHard>(3);
+      playerBoards[playerTurn]->setHeavy(true);
+    } else {
+      playerLevels[playerTurn] = make_shared<LevelHard>(4);
+      playerBoards[playerTurn]->setObstacle(true);
+    }
+  }
+  playerBoards[playerTurn]->setLevel(playerLevel + 1);
+}
 
-// // implement a level factory plz, and board or player has to have
-// // a factory field
-// void Game::levelDown() {
-//   int playerLevel = playerBoards[playerTurn]->getLevel();
-//   if (playerLevel == 0) {
-//     return;
-//   } else {
-//     if (playerLevel == 1) {
-//       if (playerTurn) {
-//         playerLevels[playerTurn] = make_shared<LevelZero>("sequence2.txt");
-//       } else {
-//         playerLevels[playerTurn] = make_shared<LevelZero>("sequence1.txt");
-//       } 
-//     } else if (playerLevel == 2) {
-//        playerLevels[playerTurn] = make_shared<LevelOne>();
-//     } else if (playerLevel == 3) {
-//       playerLevels[playerTurn] = make_shared<LevelTwo>();
-//       playerBoards[playerTurn]->setHeavy(false);
-//       // turn off heavy
-//     } else {
-//       playerLevels[playerTurn] = make_shared<LevelHard>(3);
-//       playerBoards[playerTurn]->setObstacle(false);
-//       // turn off * function;
-//     }
-//   }
-//   playerBoards[playerTurn]->setLevel(playerLevel - 1);
-// }
+// implement a level factory plz, and board or player has to have
+// a factory field
+void Game::levelDown() {
+  int playerLevel = playerBoards[playerTurn]->getLevel();
+  if (playerLevel == 0) {
+    return;
+  } else {
+    if (playerLevel == 1) {
+      if (playerTurn) {
+        playerLevels[playerTurn] = make_shared<LevelZero>("sequence2.txt");
+      } else {
+        playerLevels[playerTurn] = make_shared<LevelZero>("sequence1.txt");
+      } 
+    } else if (playerLevel == 2) {
+       playerLevels[playerTurn] = make_shared<LevelOne>();
+    } else if (playerLevel == 3) {
+      playerLevels[playerTurn] = make_shared<LevelTwo>();
+      playerBoards[playerTurn]->setHeavy(false);
+      // turn off heavy
+    } else {
+      playerLevels[playerTurn] = make_shared<LevelHard>(3);
+      playerBoards[playerTurn]->setObstacle(false);
+      // turn off * function;
+    }
+  }
+  playerBoards[playerTurn]->setLevel(playerLevel - 1);
+}
 
-// void Game::playerPlay() {
-//   cout << "Enter your moves: " << endl;
-//   // cin.exceptions(ios::eofbit|ios::failbit);
-//   shared_ptr<Board> bInPlay = playerBoards[playerTurn];
-//   string cmd;
-//   Xwindow wd;
-//   // try {
-//     while (cin >> cmd) {  
-//         cout << "reading commands" << endl;
-//         if (bInPlay->isLose()) {
-//             cout << "LOSE! END GAME!" << endl;
-//             restartGame();
-//         } else if (cmd == "left") {
-//           bInPlay->moveBlock(cmd);
-//           cout << *bInPlay;
-//         } else if (cmd == "right") {
-//           cout << "correct command read in" << endl;
-//           bInPlay->moveBlock(cmd);
-//           cout << *bInPlay;
-//         } else if (cmd == "down") {
-//           bInPlay->moveBlock(cmd);
-//           cout << *bInPlay;
-//         } else if (cmd == "clockwise") {
-//           bInPlay->moveBlock(cmd);
-//           cout << *bInPlay;
-//         } else if (cmd == "counterclockwise") {
-//           bInPlay->moveBlock(cmd);
-//           cout << *bInPlay;
-//         } else if (cmd == "drop") {
-//           bInPlay->moveBlock("save");
-//           bInPlay->dropBlock();
-//           if (bInPlay->isLose()) {
-//             cout << "LOSE! END GAME!" << endl;
-//             restartGame();
-//           }
-//           bInPlay->updateScore();
-//           cout << *bInPlay;
-//           playerBoards[playerTurn]->moveBlock("");
-//           playerLevels[playerTurn]->newMove(bInPlay);
-//           playerTurn = !playerTurn;
-//         } else if (cmd == "levelup") {
-//           levelUp();
-//         } else if (cmd == "leveldown") {
-//           levelDown();
-//         } else {
-//           break;
-//         } // if
-//       } // while
-//   // } catch (ios::failure&) {}
-// }
+void Game::playerPlay() {
+  cout << "Enter your moves: " << endl;
+  // cin.exceptions(ios::eofbit|ios::failbit);
+  shared_ptr<Board> bInPlay = playerBoards[playerTurn];
+  string cmd;
+  Xwindow wd;
+  // try {
+    while (cin >> cmd) {  
+        cout << "reading commands" << endl;
+        if (bInPlay->isLose()) {
+            cout << "LOSE! END GAME!" << endl;
+            restartGame();
+        } else if (cmd == "left") {
+          bInPlay->moveBlock(cmd);
+          cout << *bInPlay;
+        } else if (cmd == "right") {
+          cout << "correct command read in" << endl;
+          bInPlay->moveBlock(cmd);
+          cout << *bInPlay;
+        } else if (cmd == "down") {
+          bInPlay->moveBlock(cmd);
+          cout << *bInPlay;
+        } else if (cmd == "clockwise") {
+          bInPlay->moveBlock(cmd);
+          cout << *bInPlay;
+        } else if (cmd == "counterclockwise") {
+          bInPlay->moveBlock(cmd);
+          cout << *bInPlay;
+        } else if (cmd == "drop") {
+          bInPlay->moveBlock("save");
+          bInPlay->dropBlock();
+          if (bInPlay->isLose()) {
+            cout << "LOSE! END GAME!" << endl;
+            restartGame();
+          }
+          bInPlay->updateScore();
+          cout << *bInPlay;
+          playerBoards[playerTurn]->moveBlock("");
+          playerLevels[playerTurn]->newMove(bInPlay);
+          playerTurn = !playerTurn;
+        } else if (cmd == "levelup") {
+          levelUp();
+        } else if (cmd == "leveldown") {
+          levelDown();
+        } else {
+          break;
+        } // if
+      } // while
+  // } catch (ios::failure&) {}
+}
 
--- game.h
+++ game.h
@@ -1,22 +1,22 @@
-// #include <memory>
-// #include "board.h"
-// #include "blockgenerator.h"
-// #include "levelzero.h"
-// #include "levelone.h"
-// #include "leveltwo.h"
-// #include "levelhard.h"
+#include <memory>
+#include "board.h"
+#include "blockgenerator.h"
+#include "levelzero.h"
+#include "levelone.h"
+#include "leveltwo.h"
+#include "levelhard.h"
 
 
-// class Game {
-//   vector<shared_ptr<Board>> playerBoards;
-//   vector<shared_ptr<BlockGenerator>> playerLevels;
-//   bool playerTurn;
+class Game {
+  vector<shared_ptr<Board>> playerBoards;
+  vector<shared_ptr<BlockGenerator>> playerLevels;
+  bool playerTurn;
 
-//   public:
-//     Game(Xwindow &wd1, Xwindow &wd2);
-//     void restartGame();
-//     void startGame();
-//     void levelUp();
-//     void levelDown();
-//     void playerPlay();
-// };
+  public:
+    Game(Xwindow &wd1, Xwindow &wd2);
+    void restartGame();
+    void startGame();
+    void levelUp();
+    void levelDown();
+    void playerPlay();
+};
